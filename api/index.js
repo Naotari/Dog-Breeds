eaafe22a420bbb7337883c2fea0bd1e27f3a635e
Frandel Rodriguez <frandel321@hotmail.com>
2023-01-09 15:54:27 -0600

    BACK: FIX changed the variables to conect to the DB

--- api/index.js
+++ api/index.js
@@ -25,6 +25,6 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => { //force true para volver a crear la DB 
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
